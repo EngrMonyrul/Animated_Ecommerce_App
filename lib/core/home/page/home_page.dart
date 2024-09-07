d3338dc917739f75f1bef7e4d63a28cf6d89cafa
Monirul <engrmonirulislam513@gmail.com>
2024-09-07 16:41:09 +0600

    common

--- lib/core/home/page/home_page.dart
+++ lib/core/home/page/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:ecommerce_landing_page/utils/extensions/context_ext.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:math' as math;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,11 +12,13 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   /*<<----------->> variables <<----------->>*/
   late AnimationController _entryAnimationController;
   late Animation _entryAnimation;
+
+  late AnimationController _sliderAnimationController;
+  late Animation _sliderAnimation;
 
   /*<<----------->> methods <<----------->>*/
   void entryAnimationControllerInit() {
@@ -35,6 +40,25 @@ class _HomePageState extends State<HomePage>
     );
   }
 
+  void sliderAnimationControllerInit() {
+    _sliderAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 400,
+      ),
+    );
+
+    _sliderAnimation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _sliderAnimationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
   void initAnimation() {
     Future.delayed(
       const Duration(seconds: 02),
@@ -44,9 +68,15 @@ class _HomePageState extends State<HomePage>
     );
   }
 
+  void setSystemUi() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  }
+
   @override
   void initState() {
+    setSystemUi();
     entryAnimationControllerInit();
+    sliderAnimationControllerInit();
     initAnimation();
     super.initState();
   }
@@ -54,6 +84,7 @@ class _HomePageState extends State<HomePage>
   @override
   void dispose() {
     _entryAnimationController.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     super.dispose();
   }
 
@@ -82,9 +113,117 @@ class _HomePageState extends State<HomePage>
                 clipBehavior: Clip.none,
                 alignment: Alignment.bottomCenter,
                 children: [
-                  Text(
-                    "Monirul Islam",
-                    style: theme.textTheme.displayLarge,
+                  Align(
+                    alignment: Alignment.center,
+                    child: Transform.rotate(
+                      angle: math.pi / 2,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          /*<<----------->> slider items <<----------->>*/
+                          for (int i = 0; i < 3; i++)
+                            Transform.scale(
+                              scale: 3.5,
+                              child: AnimatedBuilder(
+                                animation: _sliderAnimation,
+                                builder: (context, child) {
+                                  return Transform.translate(
+                                    offset: Offset(
+                                      i == 1
+                                          ? (-25.h +
+                                              (100.h * _sliderAnimation.value))
+                                          : -100.h * _sliderAnimation.value,
+                                      0,
+                                    ),
+                                    child: Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 40.h),
+                                      child: Opacity(
+                                        opacity: 1.0 - _sliderAnimation.value,
+                                        child: Text(
+                                          "EMARKET",
+                                          style: TextStyle(
+                                            fontSize: 50,
+                                            color: theme.colorScheme.onSurface
+                                                .withOpacity(0.15),
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 100.h,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: theme.colorScheme.background,
+                              offset: const Offset(0, 0),
+                              blurRadius: 300,
+                              spreadRadius: 20,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                        height: 100.h,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: theme.colorScheme.background,
+                              offset: const Offset(0, 0),
+                              blurRadius: 300,
+                              spreadRadius: 20,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SafeArea(
+                    child: AnimatedBuilder(
+                      animation: _sliderAnimation,
+                      builder: (context, child) {
+                        return Column(
+                          children: [
+                            SizedBox(
+                              height: 20.h,
+                            ),
+                            Opacity(
+                              opacity: 1.0 - _sliderAnimation.value,
+                              child: const Column(
+                                children: [
+                                  Text(
+                                    "EMARKET",
+                                    style: TextStyle(
+                                      fontSize: 35,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Text(
+                                    "Clothing Store App",
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
                   ),
                 ],
               ),
--- lib/utils/themes/text_theme_config.dart
+++ lib/utils/themes/text_theme_config.dart
@@ -5,34 +5,34 @@ class TextThemeConfig {
 
   static final lightTextThemeConfig = TextTheme(
     labelSmall: const TextStyle().copyWith(
-        fontSize: 8, fontWeight: FontWeight.bold, color: Colors.black),
+        fontSize: 8, fontWeight: FontWeight.bold, color: Colors.white),
     labelMedium: const TextStyle().copyWith(
-        fontSize: 10, fontWeight: FontWeight.normal, color: Colors.black),
+        fontSize: 10, fontWeight: FontWeight.normal, color: Colors.white),
     labelLarge: const TextStyle().copyWith(
-        fontSize: 12, fontWeight: FontWeight.w600, color: Colors.black),
+        fontSize: 12, fontWeight: FontWeight.w600, color: Colors.white),
     bodySmall: const TextStyle().copyWith(
-        fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black),
+        fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white),
     bodyMedium: const TextStyle().copyWith(
-        fontSize: 14, fontWeight: FontWeight.normal, color: Colors.black),
+        fontSize: 14, fontWeight: FontWeight.normal, color: Colors.white),
     bodyLarge: const TextStyle().copyWith(
-        fontSize: 16, fontWeight: FontWeight.w600, color: Colors.black),
+        fontSize: 16, fontWeight: FontWeight.w600, color: Colors.white),
     titleSmall: const TextStyle().copyWith(
-        fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black),
+        fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
     titleMedium: const TextStyle().copyWith(
-        fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),
+        fontSize: 18, fontWeight: FontWeight.normal, color: Colors.white),
     titleLarge: const TextStyle().copyWith(
-        fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black),
+        fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white),
     headlineSmall: const TextStyle().copyWith(
-        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
     headlineMedium: const TextStyle().copyWith(
-        fontSize: 22, fontWeight: FontWeight.normal, color: Colors.black),
+        fontSize: 22, fontWeight: FontWeight.normal, color: Colors.white),
     headlineLarge: const TextStyle().copyWith(
-        fontSize: 24, fontWeight: FontWeight.w600, color: Colors.black),
+        fontSize: 24, fontWeight: FontWeight.w600, color: Colors.white),
     displaySmall: const TextStyle().copyWith(
-        fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+        fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
     displayMedium: const TextStyle().copyWith(
-        fontSize: 28, fontWeight: FontWeight.normal, color: Colors.black),
+        fontSize: 28, fontWeight: FontWeight.normal, color: Colors.white),
     displayLarge: const TextStyle().copyWith(
-        fontSize: 34, fontWeight: FontWeight.w600, color: Colors.black),
+        fontSize: 34, fontWeight: FontWeight.w600, color: Colors.white),
   );
 }
